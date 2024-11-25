ad38d243bab9fec6a876b46a01147874ce383716
dre4ft <dreaft@proton.me>
2024-11-25 11:31:20 +0100

    Update scanner.go

---
+++ scanner.go
@@ -0,0 +1,43 @@
+
+package main
+
+// 2. Importation des packages nécessaires
+import (
+	"net"
+    "fmt"
+	"time"
+)
+   
+
+func scanner(protocol string , address string, portRange []int, timeout int ) map[string]bool {
+
+	toReturn := make(map[string]bool)
+	duration := time.Duration(timeout) * time.Second
+
+	for _,port := range portRange{
+
+		ip_port := fmt.Sprintf("%s:%d", address, port)
+		protocol_port := fmt.Sprintf("%s,%d", protocol, port)
+
+		conn, err := net.DialTimeout(protocol, ip_port, duration)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+				fmt.Println("Timeout occurred")
+			} else {
+				fmt.Println("Error:", err)
+			}
+			
+			toReturn[protocol_port] = false 
+		}
+		defer conn.Close()
+		toReturn[protocol_port] = true  
+
+	}
+
+	return toReturn
+}
+
+func toString(input map[string]bool){
+
+
+}
