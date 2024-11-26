99ada7c79ea0373a75e2059419ef00b006357e7a
dre4ft <dreaft@proton.me>
2024-11-26 11:44:27 +0100

    update scanner

--- scanner.go
+++ scanner.go
@@ -9,32 +9,31 @@ import (
 )
    
 
-func scanner(protocol string , address string, portRange []int, timeout int ) map[string]bool {
+func scanner(protocol string, address string, portRange []int, timeout int) map[string]bool {
+    toReturn := make(map[string]bool)
+    duration := time.Duration(timeout) * time.Second
 
-	toReturn := make(map[string]bool)
-	duration := time.Duration(timeout) * time.Second
+    for _, port := range portRange {
+        ip_port := fmt.Sprintf("%s:%d", address, port)
+        protocol_port := fmt.Sprintf("%s,%d", protocol, port)
+        
+    
+        conn, err := net.DialTimeout(protocol, ip_port, duration)
+        
+        if err != nil {
+           
+            toReturn[protocol_port] = false
+            continue
+        }
+     
+        conn.Close()
+        
+       
+        toReturn[protocol_port] = true
+    }
 
-	for _,port := range portRange{
-
-		ip_port := fmt.Sprintf("%s:%d", address, port)
-		protocol_port := fmt.Sprintf("%s,%d", protocol, port)
-
-		conn, err := net.DialTimeout(protocol, ip_port, duration)
-		if err != nil {
-			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
-				fmt.Println("Timeout occurred")
-			}
-	
-			toReturn[protocol_port] = false 
-		}
-		defer conn.Close()
-		toReturn[protocol_port] = true  
-
-	}
-
-	return toReturn
+    return toReturn
 }
-
 func toString(input map[string]bool){
 
 
