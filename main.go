907d1e12c260b0b8ad547c59c3032ac6005e72c4
dre4ft <dreaft@proton.me>
2024-11-26 13:20:21 +0100

    update main to use url

--- main.go
+++ main.go
@@ -6,36 +6,39 @@ import (
 )
 
 func main() {
-	// Définir les options
-	ptcl := flag.String("type","tcp","the protocol to use ")
+	ptcl := flag.String("type", "tcp", "the protocol to use ")
 	host := flag.String("host", "127.0.0.1", "The target host to scan")
 	ports := flag.String("ports", "20-1000", "list of ports to scan ex : 80,443 or 0-100")
 	timeout := flag.Int("timeout", 5, "Timeout in seconds for the connection")
 	flag.Parse()
 
+	portRange, err := parsePorts(*ports)
 
-
-	port_range, err := parsePorts(*ports)
-	//checkAddr = checkIP(*host)
-	//checkPtcl = checkProtocole(*ptcl) 
-
-	if !checkIP(*host){
-		fmt.Println("Error with the host address")
-		return
+	var finalHost string
+	if checkIP(*host) {
+		finalHost = *host
+	} else {
+		if matched, _ := regexp.MatchString(`^([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$`, *host); matched {
+			ip, err := reverseLookup(*host)
+			if err != nil {
+				fmt.Printf("Erreur de reverse lookup pour l'URL %s : %v\n", *host, err)
+				return
+			}
+			finalHost = ip
+		} else {
+			fmt.Println("Erreur : L'adresse ou l'URL fournie n'est pas valide.")
+			return
+		}
 	}
+
 	if !checkProtocole(*ptcl) {
 		fmt.Println("Error with the protocol")
-		return 
+		return
 	}
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	//result := whosup(*host, port_range, *timeout)
-	//toString(result)
-	scanAndGrab(*host, port_range, *timeout)
-	
-
-
+	scanAndGrab(finalHost, portRange, *timeout)
 }
