bdaac69ef9da0c05f1ae2bfe37394577ddbe45df
dre4ft <dreaft@proton.me>
2024-11-30 18:07:16 +0100

    add file

--- scanner.go
+++ scanner.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	//"context"
+	"context"
     "fmt"
     "net"
-    //"sync"
+    "sync"
     "time"
 )
 
@@ -22,72 +22,6 @@ type ScanResult struct {
     Banner  string
 }
 
-/*func parallelScan(ip string, ports []int, timeout int, grab bool) map[string]string {
-    results := make(map[string]string)
-    resultsChan := make(chan ScanResult, len(ports))
-    errChan := make(chan error, 1)
-    var wg sync.WaitGroup
-    var mu sync.Mutex
-    semaphore := make(chan struct{}, 100)
-
-    ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
-    defer cancel()
-
-    sentPorts := make(map[int]bool)
-
-    for _, port := range ports {
-        wg.Add(1)
-        semaphore <- struct{}{}
-
-        go func(port int) {
-            defer wg.Done()
-            defer func() { <-semaphore }()
-            conn, err := (&net.Dialer{
-                Timeout: time.Duration(timeout) * time.Second,
-            }).DialContext(ctx, "tcp", fmt.Sprintf("%s:%d", ip, port))
-            
-            if err != nil {
-                return
-            }
-            defer conn.Close()
-
-            result := ScanResult{
-                Port:   port,
-                State:  "open",
-                Banner: "",
-            }
-            if grab {
-                result.Banner = grabBanner(conn, port)
-            }
-            select {
-            case resultsChan <- result:
-            case <-ctx.Done():
-                return
-            }
-        }(port)
-    }
-
-    go func() {
-        wg.Wait()
-        close(resultsChan)
-        close(errChan)
-    }()
-
-    for result := range resultsChan {
-        mu.Lock()
-        if !sentPorts[result.Port] {
-            protPort := fmt.Sprintf("%d/tcp", result.Port)
-            if result.Banner != "" {
-                results[protPort] = fmt.Sprintf("open\n%s", result.Banner)
-            } else {
-                results[protPort] = "open"
-            }
-            sentPorts[result.Port] = true
-        }
-        mu.Unlock()
-    }
-    return results
-}*/
 
 
 func scan(ip string, ports []int, timeout int, grab bool) map[string]string {
