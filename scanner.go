14cfa7512554995c2d44bfcb6179e98cb086da60
dre4ft <dreaft@proton.me>
2024-11-30 18:47:41 +0100

    _

--- scanner.go
+++ scanner.go
@@ -100,8 +100,8 @@ func scan(ip string, ports []int, timeout int, grab bool) map[string]string {
 // Convertit une map en une chaîne lisible.
 func toString(input map[string]string) {
 	for key, value := range input {
-		cve := SearchAndFormat(value)
-		fmt.Printf("%s : %s \n", key,cve)
+		//cve := SearchAndFormat(value)
+		fmt.Printf("%s : %s \n", key,value)
 	}
 }
 
